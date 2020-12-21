9e47d3203c7d6a4fb5208fd13363d94c95840b9d
tomoyan <7days-tn@msn.com>
2020-12-21 10:39:46 -0800

    add service account config

--- BlurtChain.py
+++ BlurtChain.py
@@ -14,12 +14,24 @@ import requests
 import pyrebase
 
 # Firebase configuration
+serviceAccountCredentials = {
+    "type": "service_account",
+    "project_id": "blurtdb",
+    "private_key_id": Config.FB_PRIVATE_KEY_ID,
+    "private_key": Config.FB_PRIVATE_KEY,
+    "client_email": Config.FB_CLIENT_EMAIL,
+    "client_id": Config.FB_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": Config.FB_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": Config.FB_CLIENT_X509_CERT_URL
+}
 firebase_config = {
     "apiKey": Config.FB_APIKEY,
     "authDomain": Config.FB_AUTHDOMAIN,
     "databaseURL": Config.FB_DATABASEURL,
     "storageBucket": Config.FB_STORAGEBUCKET,
-    # "serviceAccount": Config.FB_SERVICEACCOUNT,
+    "serviceAccount": serviceAccountCredentials,
 }
 # Firebase initialization
 firebase = pyrebase.initialize_app(firebase_config)
--- config.py
+++ config.py
@@ -14,5 +14,12 @@ class Config(object):
     FB_AUTHDOMAIN = 'blurtdb.firebaseapp.com'
     FB_DATABASEURL = 'https://blurtdb.firebaseio.com'
     FB_STORAGEBUCKET = 'blurtdb.appspot.com'
-    # FB_SERVICEACCOUNT = os.environ.get(
-    #     'FB_SERVICEACCOUNT') or 'YOUR_SERVICEACCOUNT'
+    FB_PRIVATE_KEY_ID = os.environ.get(
+        'FB_PRIVATE_KEY_ID') or 'YOUR_PRIVATE_KEY_ID'
+    FB_PRIVATE_KEY = os.environ.get('FB_PRIVATE_KEY') or 'YOUR_PRIVATE_KEY'
+    FB_CLIENT_EMAIL = os.environ.get('FB_CLIENT_EMAIL') or 'YOUR_CLIENT_EMAIL'
+    FB_CLIENT_ID = os.environ.get('FB_CLIENT_ID') or 'YOUR_CLIENT_ID'
+    FB_AUTH_PROVIDER_X509_CERT_URL = os.environ.get(
+        'FB_AUTH_PROVIDER_X509_CERT_URL') or 'YOUR_AUTH_PROVIDER_X509_CERT_URL'
+    FB_CLIENT_X509_CERT_URL = os.environ.get(
+        'FB_CLIENT_X509_CERT_URL') or 'YOUR_CLIENT_X509_CERT_URL'
