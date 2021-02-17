74defab7c0c51f303f2c7b63221b83098b55be42
tomoyan <7days-tn@msn.com>
2021-02-17 07:18:31 -0800

    update reward %

--- tasks/delegation_reward.py
+++ tasks/delegation_reward.py
@@ -47,8 +47,8 @@ def get_reward_budget():
     # Get 1 day curation reward in BP
     reward_bp = account.get_curation_reward(days=1)
 
-    # 30 % of reward gets distributed
-    percent = 30
+    # 25 % of reward gets distributed
+    percent = 25
     budget_bp = int(reward_bp * percent / 100)
 
     return budget_bp
