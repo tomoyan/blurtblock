d29d8383a5693760c00aff1b344107415ce894c5
tomoyan <7days-tn@msn.com>
2021-09-07 14:07:00 -0700

    add upvote_blog_entries_username

--- tasks/upvote_blog_entries.py
+++ tasks/upvote_blog_entries.py
@@ -15,6 +15,11 @@ def main():
     # Script runs after daily delegation report
     upvote_blog_entries()
 
+    # 100% Upvote for special blogs
+    users = ['ecosynthesizer', 'maxinpower']
+    for user in users:
+        upvote_blog_entries_username(user)
+
 
 def upvote_blog_entries():
     # Get my last 7 blog posts
@@ -30,6 +35,20 @@ def upvote_blog_entries():
             identifier = post.authorperm
             # Upvote a post
             BLURT.vote(weight, identifier, account=ACCOUNT)
+
+
+def upvote_blog_entries_username(name):
+    ACCT = Account(name, blockchain_instance=BLURT)
+    posts = ACCT.blog_history(limit=7, reblogs=False)
+
+    for post in posts:
+        voted = ACCT.has_voted(post)
+
+        if not voted:
+            weight = 100.0
+            identifier = post.authorperm
+            # Upvote a post
+            BLURT.vote(weight, identifier, account=ACCT)
 
 
 if __name__ == '__main__':
