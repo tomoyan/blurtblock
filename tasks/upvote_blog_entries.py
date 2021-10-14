da8d532e34b9dc53c53ed215f0b03753492ae995
tomoyan <7days-tn@msn.com>
2021-10-14 14:45:52 -0700

    upvote_blog_entries clean up

--- tasks/upvote_blog_entries.py
+++ tasks/upvote_blog_entries.py
@@ -9,32 +9,13 @@ UPVOTE_KEY = os.environ.get('POST_KEY')
 BLURT = Blurt(BLURT_NODES, keys=[UPVOTE_KEY])
 ACCOUNT = Account(USERNAME, blockchain_instance=BLURT)
 
+# 100% Upvote for special users
+USERS = ['ecosynthesizer', 'maxinpower']
+
 
 def main():
-    # Upvote my own posts automatically
-    # Script runs after daily delegation report
-    # upvote_blog_entries()
-
-    # 100% Upvote for special blogs
-    users = ['ecosynthesizer', 'maxinpower']
-    for user in users:
+    for user in USERS:
         upvote_blog_entries_username(user)
-
-
-# def upvote_blog_entries():
-#     # Get my last 7 blog posts
-#     # Upvote them if they are not voted
-#     posts = ACCOUNT.blog_history(limit=7, reblogs=False)
-
-#     for post in posts:
-#         voted = ACCOUNT.has_voted(post)
-
-#         # 100% Upvote if post hasn't been voted
-#         if not voted:
-#             weight = 100.0
-#             identifier = post.authorperm
-#             # Upvote a post
-#             BLURT.vote(weight, identifier, account=ACCOUNT)
 
 
 def upvote_blog_entries_username(name):
@@ -43,7 +24,6 @@ def upvote_blog_entries_username(name):
 
     for post in posts:
         voted = ACCOUNT.has_voted(post)
-        # print(name, post, voted)
 
         if not voted:
             weight = 100.0
