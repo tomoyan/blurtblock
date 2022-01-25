9335da28a5f5b8c99a541f82ae0ba64dda151ee0
tomoyan <7days-tn@msn.com>
2022-01-25 15:35:33 -0800

    change endpoint

--- app.py
+++ app.py
@@ -401,14 +401,14 @@ def curation_trail_vote():
 
 
 @app.route('/api/blurt/msg-token', methods=['POST'])
-@app.route('/api/blurt/msg-token/', methods=['POST'])
+# @app.route('/api/blurt/msg-token/', methods=['POST'])
 def blurt_msg_token():
     data = {}
 
     if request.method == 'POST':
         token = request.form.get('token')
         api_key = request.form.get('api_key')
-        print(token, api_key)
+        print('BLURT_MSG_TOKEN', token, api_key)
 
         blurt = BC.BlurtChain(username=None)
         data = blurt.get_msg_token(api_key, token)
