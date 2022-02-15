df80b2b06da114414129c8fa0d31c689f2c6b859
tomoyan <7days-tn@msn.com>
2022-02-15 14:02:33 -0800

    update node

--- BlurtChain.py
+++ BlurtChain.py
@@ -36,7 +36,7 @@ firebase = pyrebase.initialize_app(firebase_config)
 BLURT_NODES = [
     'https://rpc.blurt.world',
     'https://blurt-rpc.saboin.com',
-    # 'https://rpc.tekraze.com',
+    'https://rpc.tekraze.com',
     'https://rpc.dotwin1981.de',
     'https://rpc.nerdtopia.de',
     'https://kentzz.blurt.world',
