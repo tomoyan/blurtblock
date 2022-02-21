f61eccd7cc4e1e843c7c1a4388cc8762f5aedd1e
tomoyan <7days-tn@msn.com>
2022-02-21 11:19:32 -0800

    update validation

--- forms.py
+++ forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, ValidationError
+import re
+
+
+def username_check(form, field):
+    pattern = '^[a-zA-Z0-9-.]+$'
+    if not bool(re.match(pattern, field.data)):
+        raise ValidationError('Invalid username')
 
 
 class UserNameForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
+    username = StringField('username', validators=[
+                           DataRequired(), username_check])
 
 
 class postUrlForm(FlaskForm):
